3ef36a8eac30bfe91d535d481de3118659f16baf
Sadeesh Radhakrishnan <sadeesh.r@gmail.com>
2018-12-24 00:18:10 +0800

    update DB connector of go_agent_sessions

--- goAgent/goLoginUser.php
+++ goAgent/goLoginUser.php
@@ -107,7 +107,7 @@ if ($sipIsLoggedIn || $use_webrtc) {
     
     $query = $astDB->insert('web_client_sessions', array('extension' => $extension, 'server_ip' => $phone_settings->server_ip, 'program' => 'vicidial', 'start_time' => $NOW_TIME, 'session_name' => $session_name));
     
-    $query = $astDB->insert('go_agent_sessions', array('sess_agent_user' => $user ,'sess_agent_phone' => $phone_login ,'sess_agent_status' => 'INUSE'));
+    $query = $goDB->insert('go_agent_sessions', array('sess_agent_user' => $user ,'sess_agent_phone' => $phone_login ,'sess_agent_status' => 'INUSE'));
     
     $astDB->where('campaign_id', $campaign);
     $query = $astDB->getOne('vicidial_hopper', 'count(*) AS cnt');
